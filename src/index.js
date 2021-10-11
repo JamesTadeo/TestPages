e3df4bd83de31e48613e230941018eead139474c
JamesTadeo <jtadeo@gmail.com>
2021-10-11 11:09:03 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+//The simplest Reactjs example
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import styles from "./styles.css";
+
+ReactDOM.render(
+  <div>
+    <h1>Things to Learn in 2021</h1>
+    <ul>
+      <li>React.js</li>
+      <li>Node.js</li>
+      <li>Mongo.js</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
